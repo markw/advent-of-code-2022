0360d1b3267a009d85410181183f5e143c42304a
Mark Williams <mwilliams@cj.com>
2022-12-04 07:57:50 -0800

    fix typo

--- 04/main.clj
+++ 04/main.clj
@@ -20,7 +20,7 @@
 
 (def overlaps? (complement disjunct?))
 
-(def pair (map parse-pair (.split (slurp "input.txt") "\n")))
+(def pairs (map parse-pair (.split (slurp "input.txt") "\n")))
 
 (assert (disjunct? (parse-pair "1-5,6-11")))
 (assert (disjunct? (parse-pair "6-11,1-5")))
@@ -28,6 +28,6 @@
 (assert (overlaps? (parse-pair "1-7,7-11")))
 (assert (overlaps? (parse-pair "83-89,9-83")))
 
-(println "part 1" (count (filter fully-contains? pair)))
-(println "part 2" (count (filter overlaps? pair)))
+(println "part 1" (count (filter fully-contains? pairs)))
+(println "part 2" (count (filter overlaps? pairs)))
 
