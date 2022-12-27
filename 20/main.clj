38fa5d72fca8e8a7849156b9e531b152b34d7be1
Mark Williams <mwilliams@cj.com>
2022-12-27 15:06:27 -0800

    day 20 switch to Java ArrayList 10x improvement

--- 20/main.clj
+++ 20/main.clj
@@ -1,34 +1,18 @@
 (defn parse-input
   [file-name]
-  (vec 
-    (map-indexed #(vector (Integer. %2) %1) 
-                 (.split (slurp file-name) "\n"))))
+  (map-indexed #(vector (Integer. %2) %1) 
+              (.split (slurp file-name) "\n")))
 
 (defn move
-  [coll index x]
-  (let [max-index (dec (count coll))
+  [alist index entry]
+  (let [max-index (dec (count alist))
+        x (first entry)
         temp-index (rem (+ index x) max-index)
-        new-index (if (neg? temp-index) (+ temp-index max-index) temp-index)
-        distance (- new-index index)]
-    (loop [mixed coll i index d distance]
-      (cond
-        (zero? d) mixed
-
-        (neg? d)
-        (recur
-          (assoc mixed 
-                 i (get mixed (dec i))
-                 (dec i) (get mixed i))
-          (dec i)
-          (inc d))
-
-        :else
-        (recur
-          (assoc mixed 
-                 i (get mixed (inc i))
-                 (inc i) (get mixed i))
-          (inc i)
-          (dec d))))))
+        new-index (if (neg? temp-index) (+ temp-index max-index) temp-index)]
+    (doto alist
+      (.remove entry)
+      (.add new-index entry))
+    alist))
 
 (defn mix
   ([coll] (mix coll coll))
@@ -41,42 +25,42 @@
            t
            (move mixed 
                  (.indexOf mixed h)
-                 (first h))))))))
+                 h)))))))
 
 (defn find-zero
-  [coll]
-  (loop [[h & t] coll n 0]
-    (if (zero? (first h))
+  [alist]
+  (loop [n 0]
+    (if (zero? (first (.get alist n)))
       n
-      (recur t (inc n)))))
+      (recur (inc n)))))
 
+(time
+  (let [coll (parse-input "input.txt")
+        mixed (mix coll (java.util.ArrayList. coll))
+        zero-index (find-zero mixed)]
+    (println "part 1" 
+             (reduce
+               (fn [acc index]
+                 (+ acc (first (.get mixed (rem index (count coll))))))
+               0
+               (mapv 
+                 #(+ zero-index %) 
+                 [1000 2000 3000])))))
 
-(let [coll (parse-input "input.txt")
-      mixed (mix coll)
-      zero-index (find-zero mixed)]
-  ;(println "zero-index" zero-index)
-  (println "part 1" 
-           (reduce
-             (fn [acc index]
-               (+ acc (first (get mixed (rem index (count coll))))))
-             0
-             (mapv 
-               #(+ zero-index %) 
-               [1000 2000 3000]))))
-
-(let [coll (mapv 
-             (fn [[value index]] [(* value 811589153) index])
-             (parse-input "input.txt"))
-      mixed (loop [mixed coll n 10]
-              (if (zero? n) mixed
-                (recur (mix coll mixed) (dec n))))
-      zero-index (find-zero mixed)]
-  ;(println "zero-index" zero-index)
-  (println "part 2"
-           (reduce
-             (fn [acc index]
-               (+ acc (first (get mixed (rem index (count coll))))))
-             0
-             (mapv 
-               #(+ zero-index %) 
-               [1000 2000 3000]))))
+(time
+  (let [coll (mapv 
+               (fn [[value index]] [(* value 811589153) index])
+               (parse-input "input.txt"))
+        mixed (loop [mixed (java.util.ArrayList. coll) n 10]
+                (if (zero? n) mixed
+                  (recur (mix coll mixed) (dec n))))
+        zero-index (find-zero mixed)]
+    ;(println "zero-index" zero-index)
+    (println "part 2"
+             (reduce
+               (fn [acc index]
+                 (+ acc (first (.get mixed (rem index (count coll))))))
+               0
+               (mapv 
+                 #(+ zero-index %) 
+                 [1000 2000 3000])))))
