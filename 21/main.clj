553fb14ef279b6b6ffe8bbfdd392018413e3b263
Mark Williams <mwilliams@cj.com>
2022-12-28 13:40:12 -0800

    day 21 part 2

--- 21/main.clj
+++ 21/main.clj
@@ -17,13 +17,68 @@
 
 (defn resolve-expr
   [tree k]
-  (let [expr (get tree k)]
-    (if (= 3 (count expr))
-      (evaluate 
-        (resolve-expr tree (first expr))
-        (second expr)
-        (resolve-expr tree (last expr)))
-      (Integer. (first expr)))))
+  (if (number? k)
+    k
+    (let [expr (get tree k)]
+      (if (= 3 (count expr))
+        (evaluate 
+          (resolve-expr tree (first expr))
+          (second expr)
+          (resolve-expr tree (last expr)))
+        (Long. (first expr))))))
+
+(defn parent
+  [tree s]
+  (first
+    (filter 
+      (fn [[k v]]
+        (or (= s (first v))(= s (last v))))
+      tree)))
+
+(defn all-parents
+  [tree s]
+  (loop [s s acc []]
+    (let [p (parent tree s)]
+      (if (nil? p)
+        acc
+        (recur (first p) (conj acc p))))))
+
+(defn invert
+  [op]
+  (condp = op
+    "+" "-"
+    "-" "+"
+    "*" "/"
+    "/" "*"))
+
+(defn rewrite
+  [tree [parent [left op right]] s]
+  (let [term (if (= s right) left right)
+        resolved (resolve-expr tree term)]
+    (cond
+      (= parent "root")
+      [s [resolved]]
+
+      (and (= s right)(= "/" op))
+      [s [resolved "/" parent]]
+
+      (and (= s right)(= "-" op))
+      [s [resolved "-" parent]]
+
+      :else
+      [s [parent (invert op) resolved]])))
+
+(defn rewrite-all
+  [tree solve-for]
+  (loop [[h & t] (all-parents tree solve-for)  solve-for solve-for acc []]
+    (if (nil? h)
+      (into {} acc)
+      (let [new-expr (rewrite tree h solve-for)
+            [parent' [left' op' right']] new-expr]
+        (if (number? left')
+          (recur t right' (conj acc new-expr))
+          (recur t left' (conj acc new-expr)))))))
            
 (let [input (parse-input "input.txt")]
-  (println "part 1" (resolve-expr input "root")))
+  (time (println "part 1" (resolve-expr input "root")))
+  (time (println "part 2" (resolve-expr (rewrite-all input "humn") "humn"))))
